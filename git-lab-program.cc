baf3c7658f6abc540455536394ef7dce2d5974e7
gula <gh824514@0hio.edu>
2019-05-16 09:13:15 -0400

    hello

---
+++ git-lab-program.cc
@@ -0,0 +1,17 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: gulakhshan
+ *        Date: 05/17/2019
+ * Description: hello world
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+    cout << "Hello Git!!" << endl;    /*add code*/
+    return 0;
+}// main
